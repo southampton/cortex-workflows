9b8b7d81de283d4f61593d4bf3dc3dd002ed6be8
Clayton Peters <me@claytonpeters.co.uk>
2015-12-18 17:35:39 +0000

    Added a bit more validation.

--- sandboxvm/views.py
+++ sandboxvm/views.py
@@ -8,12 +8,15 @@ from flask import Flask, request, session, redirect, url_for, flash, g, abort, r
 @app.workflow_handler(__name__, 'Sandbox VM', methods=['GET', 'POST'])
 @cortex.core.login_required
 def sandboxvm_create():
-
+	# Define what CPU, RAM and Disk specs we can have
 	cpu_list = [1, 2, 4, 8]
 	mem_list = [2, 4, 8, 16]
 	disk_list = [0, 50, 100]
 
+	# Get the list of clusters
 	clusters = cortex.core.vmware_list_clusters("srv01197")
+
+	# Get the list of environments
 	environments = cortex.core.get_cmdb_environments()
 
 	if request.method == 'GET':
@@ -21,6 +24,12 @@ def sandboxvm_create():
 		return render_template(__name__ + "::create.html", cpu_list=cpu_list, mem_list=mem_list, disk_list=disk_list, clusters=clusters, environments=environments)
 
 	elif request.method == 'POST':
+		# Ensure we have all parameters that we require
+		if 'cpu' not in request.form or 'ram' not in request.form or 'disk' not in request.form or 'template' not in request.form or 'cluster' not in request.form or 'environment' not in request.form:
+			flash('You must select options for all questions before creating', 'alert-danger')
+			return redirect(url_for('sandboxvm_create'))
+
+		# Extract all the parameters
 		cpu      = request.form['cpu']
 		ram      = request.form['ram']
 		disk     = request.form['disk']
@@ -28,27 +37,27 @@ def sandboxvm_create():
 		cluster  = request.form['cluster']
 		env      = request.form['environment']
 
-		# Validate CPU count
+		# Validate CPU count against our defined list
 		if int(cpu) not in cpu_list:
 			abort(400)
 
-		# Validate CPU count
+		# Validate CPU count against our defined list
 		if int(ram) not in mem_list:
 			abort(400)
 
-		# Validate disk size
+		# Validate disk size against our defined list
 		if int(disk) not in disk_list:
 			abort(400)
 
-		# Validate cluster
+		# Validate cluster against the list we've got
 		if cluster not in [c['name'] for c in clusters]:
 			abort(400)
 
-		# Validate environment
+		# Validate environment against the list we've got
 		if env not in [e['id'] for e in environments]:
 			abort(400)
 
-		# Build options
+		# Build options to pass to the task
 		options = {}
 		options['cpu'] = cpu
 		options['ram'] = ram
