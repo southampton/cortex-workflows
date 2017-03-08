11edb33df3c9d5057f6775b46c80f474fed75202
jordan <comms@sluglf.com>
2017-03-08 16:02:18 +0000

    Base student build template created

--- buildvm/views.py
+++ buildvm/views.py
@@ -9,6 +9,7 @@ from flask import Flask, request, session, redirect, url_for, flash, g, abort
 workflow = CortexWorkflow(__name__)
 workflow.add_permission('buildvm.sandbox', 'Create Sandbox VM')
 workflow.add_permission('buildvm.standard', 'Create Standard VM')
+workflow.add_permission('buildvm.student', 'Create Student VM')
 
 ################################################################################
 ## Sandbox VM Workflow view handler
@@ -147,6 +148,68 @@ def standard():
 		# Connect to NeoCortex and start the task
 		neocortex = cortex.lib.core.neocortex_connect()
 		task_id = neocortex.create_task(__name__, session['username'], options, description="Creates and sets up a virtual machine (standard VMware environment)")
+
+		# Redirect to the status page for the task
+		return redirect(url_for('task_status', id=task_id))
+
+################################################################################
+## Student VM Workflow view handler
+@workflow.route("student", title='Create Student VM', order=30, permission="buildvm.student", methods=['GET', 'POST'])
+def student():
+	# Get the list of clusters
+	clusters = cortex.lib.core.vmware_list_clusters("srv01197")
+
+	# Get the list of environments
+	environments = cortex.lib.core.get_cmdb_environments()
+
+	if request.method == 'GET':
+		## Show form
+		return workflow.render_template("student.html", clusters=clusters, environments=environments, title="Create Virtual Machine", default_env='dev', default_cluster='CHARTREUSE', os_names=workflow.config['SB_OS_DISP_NAMES'], os_order=workflow.config['SB_OS_ORDER'])
+
+	elif request.method == 'POST':
+		# Ensure we have all parameters that we require
+		if 'sockets' not in request.form or 'cores' not in request.form or 'ram' not in request.form or 'disk' not in request.form or 'template' not in request.form or 'environment' not in request.form:
+			flash('You must select options for all questions before creating', 'alert-danger')
+			return redirect(url_for('student'))
+		
+		# Form validation
+		try:
+			# Extract all the parameters
+			purpose  = request.form['purpose']
+			comments = request.form['comments']
+			sendmail = 'send_mail' in request.form
+
+			# Validate the data (common between standard / sandbox)
+			(sockets, cores, ram, disk, template, env, expiry) = validate_data(request, workflow.config['SB_OS_ORDER'], [e['id'] for e in environments])
+
+		except ValueError as e:
+			flash(str(e), 'alert-danger')
+			return redirect(url_for('student'))
+
+		except Exception as e:
+			flash('Submitted data invalid ' + str(e), 'alert-danger')
+			return redirect(url_for('student'))
+				
+		# Build options to pass to the task
+		options = {}
+		options['workflow'] = 'student'
+		options['sockets'] = sockets
+		options['cores'] = cores
+		options['ram'] = ram
+		options['disk'] = disk
+		options['template'] = template
+		#options['cluster'] = cluster	## Commenting out whilst we only have one cluster
+		options['cluster'] = 'CHARTREUSE'
+		options['env'] = env
+		options['purpose'] = purpose
+		options['comments'] = comments
+		options['expiry'] = expiry
+		options['sendmail'] = sendmail
+		options['wfconfig'] = workflow.config
+
+		# Connect to NeoCortex and start the task
+		neocortex = cortex.lib.core.neocortex_connect()
+		task_id = neocortex.create_task(__name__, session['username'], options, description="Creates and sets up a virtual machine (student VMware environment)")
 
 		# Redirect to the status page for the task
 		return redirect(url_for('task_status', id=task_id))
